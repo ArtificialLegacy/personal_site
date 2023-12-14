530ca48a0beae0fd7394cb3c87252be5c3070a22
ArtificialLegacy <redsbro457@gmail.com>
2023-12-14 10:21:55 -0500

    gradients added

--- src/modules/banner/components/Banner.tsx
+++ src/modules/banner/components/Banner.tsx
@@ -3,23 +3,25 @@ import '../banner.css'
 function Banner() {
   return (
     <div className="banner">
-      <div className="banner-pfp-container">
-        <img className="banner-pfp" src="pfp.png" />
-      </div>
+      <div className="banner-container">
+        <div className="banner-pfp-container">
+          <img className="banner-pfp" src="pfp.png" />
+        </div>
 
-      <div className="banner-card">
-        <p className="banner-text">
-          asdasfsdfsdf oihsdkfuh weuihf hwebfb sdbf sud bhcxvbh uisdbfi bs sdf sdf sdf sdf w3ee fewf wefn khwev hbdisb
-          fibdshv bsdhb jbvqwgvdBD iasjndf hbfwiuef nxchbviu sbdiufbesjhvbeqjhbwhdb iusdnfi unsdfu inisdvnhxcbv jhbbquif
-          iuajwaqnd ka skjc ajncuqw uqnwind naskc jakncjan iuwqnd nkjdnkj nnvnv unsiunefijqnkwjnekjzdnc nc;nu;an
-          wcuhainbwdb hbaiusxcuinbdib behwbd hba asdasfsdfsdf oihsdkfuh weuihf hwebfb sdbf sud bhcxvbh uisdbfi bs sdf
-          sdf sdf sdf w3ee fewf wefn khwev hbdisb fibdshv bsdhb jbvqwgvdBD iasjndf hbfwiuef nxchbviu
-          sbdiufbesjhvbeqjhbwhdb iusdnfi unsdfu inisdvnhxcbv jhbbquif iuajwaqnd ka skjc ajncuqw uqnwind naskc jakncjan
-          iuwqnd nkjdnkj nnvnv unsiunefijqnkwjnekjzdnc nc;nu;an wcuhainbwdb hbaiusxcuinbdib behwbd hba asdasfsdfsdf
-          sdfjklnsd nu wenfjn e fksndvuoixcv; ioj oij ioej ijdsoifj nweujifn uinsuid oiasjd ioj askjd wiuef nxchbviu
-          sbdiufbesjhvbeqjhbwhdb iusdnfi unsdfu inisdvnhxcbv jhbbquif iuajwaqnd ka s asdasfsdfsd d oiasjd ioj askj asd
-          asfohoiqw
-        </p>
+        <div className="banner-card">
+          <p className="banner-text">
+            asdasfsdfsdf oihsdkfuh weuihf hwebfb sdbf sud bhcxvbh uisdbfi bs sdf sdf sdf sdf w3ee fewf wefn khwev hbdisb
+            fibdshv bsdhb jbvqwgvdBD iasjndf hbfwiuef nxchbviu sbdiufbesjhvbeqjhbwhdb iusdnfi unsdfu inisdvnhxcbv
+            jhbbquif iuajwaqnd ka skjc ajncuqw uqnwind naskc jakncjan iuwqnd nkjdnkj nnvnv unsiunefijqnkwjnekjzdnc
+            nc;nu;an wcuhainbwdb hbaiusxcuinbdib behwbd hba asdasfsdfsdf oihsdkfuh weuihf hwebfb sdbf sud bhcxvbh
+            uisdbfi bs sdf sdf sdf sdf w3ee fewf wefn khwev hbdisb fibdshv bsdhb jbvqwgvdBD iasjndf hbfwiuef nxchbviu
+            sbdiufbesjhvbeqjhbwhdb iusdnfi unsdfu inisdvnhxcbv jhbbquif iuajwaqnd ka skjc ajncuqw uqnwind naskc jakncjan
+            iuwqnd nkjdnkj nnvnv unsiunefijqnkwjnekjzdnc nc;nu;an wcuhainbwdb hbaiusxcuinbdib behwbd hba asdasfsdfsdf
+            sdfjklnsd nu wenfjn e fksndvuoixcv; ioj oij ioej ijdsoifj nweujifn uinsuid oiasjd ioj askjd wiuef nxchbviu
+            sbdiufbesjhvbeqjhbwhdb iusdnfi unsdfu inisdvnhxcbv jhbbquif iuajwaqnd ka s asdasfsdfsd d oiasjd ioj askj asd
+            asfohoiqw
+          </p>
+        </div>
       </div>
     </div>
   )
