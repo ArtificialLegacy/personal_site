6fd8278123d9249d31788ecac2d1e7a4b32c42b4
ArtificialLegacy <redsbro457@gmail.com>
2023-12-30 16:12:02 -0500

    degtorad utility function

---
+++ src/utility/degtorad.ts
@@ -0,0 +1,10 @@
+/**
+ * Converts degrees to radians.
+ * @param degrees - The degrees to convert.
+ * @returns - The radians.
+ */
+function degtorad(degrees: number): number {
+  return degrees * (Math.PI / 180)
+}
+
+export default degtorad
