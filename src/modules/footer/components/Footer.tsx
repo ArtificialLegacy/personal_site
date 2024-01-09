7d3218cee8946015d5e3423b48de189131f83476
ArtificialLegacy <redsbro457@gmail.com>
2024-01-08 19:28:06 -0500

    add footer

---
+++ src/modules/footer/components/Footer.tsx
@@ -0,0 +1,14 @@
+import '../styles/footer.css'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-copyright">&#169; Name??</p>
+      <a href="https://github.com/ArtificialLegacy">
+        <img className="footer-github" src="GitHub_Logo_White.png" alt="Github Profile Link" />
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
