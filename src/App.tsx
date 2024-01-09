b9caf856c95fdf3b0756a02d1f5e926bdab2ef90
ArtificialLegacy <redsbro457@gmail.com>
2024-01-08 19:41:32 -0500

    update App component to reflect additions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Banner } from 'modules/banner'
+import { ProjectListCarousel } from 'modules/project_carousel'
+import { Footer } from 'modules/footer'
+
+import { blubify, upscaleCli, portfolio } from './projects'
 
 function App() {
   return (
     <div>
       <Banner />
+      <ProjectListCarousel projects={[blubify, upscaleCli, portfolio]} />
+      <Footer />
     </div>
   )
 }
