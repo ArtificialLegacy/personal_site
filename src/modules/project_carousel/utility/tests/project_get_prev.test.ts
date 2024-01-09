c8639156b6350052cbfb11478c5147005f26cf9c
ArtificialLegacy <redsbro457@gmail.com>
2024-01-09 07:31:48 -0500

    add some tests

---
+++ src/modules/project_carousel/utility/tests/project_get_prev.test.ts
@@ -0,0 +1,19 @@
+import projectGetPrev from '../project_get_prev'
+
+import { blubify, upscaleCli, portfolio } from 'data/projects'
+
+describe('projectGetPrev', () => {
+  const projects = [blubify, upscaleCli, portfolio]
+
+  it('should get the previous project', () => {
+    expect(projectGetPrev(0, projects)).toBe(2)
+    expect(projectGetPrev(1, projects)).toBe(0)
+    expect(projectGetPrev(2, projects)).toBe(1)
+  })
+
+  it('should get the previous project with an offset', () => {
+    expect(projectGetPrev(0, projects, 2)).toBe(1)
+    expect(projectGetPrev(1, projects, 2)).toBe(2)
+    expect(projectGetPrev(2, projects, 2)).toBe(0)
+  })
+})
---
+++ src/utility/tests/Vector.test.ts
@@ -0,0 +1,62 @@
+import Vector from '../Vector'
+
+describe('Vector instance', () => {
+  const vec = new Vector(1, 2)
+
+  it('should create a vector', () => {
+    expect(vec).toEqual({ x: 1, y: 2 })
+  })
+
+  it('should get a new vector from', () => {
+    expect(vec.from()).toEqual(vec)
+  })
+
+  it('should get the distance between two vectors', () => {
+    expect(vec.distance(new Vector(1, 4))).toBe(2)
+  })
+
+  it('should add a vector', () => {
+    expect(vec.add(new Vector(2, 3))).toEqual(new Vector(3, 5))
+  })
+
+  it('should subtract a vector', () => {
+    expect(vec.sub(new Vector(2, 3))).toEqual(new Vector(1, 2))
+  })
+
+  it('should multiply a vector', () => {
+    expect(vec.mul(new Vector(2, 3))).toEqual(new Vector(2, 6))
+  })
+
+  it('should multiply by a factor', () => {
+    expect(vec.mulBy(2)).toEqual(new Vector(4, 12))
+  })
+
+  it('should divide by a factor', () => {
+    expect(vec.divBy(2)).toEqual(new Vector(2, 6))
+  })
+})
+
+describe('Vector static', () => {
+  it('should get a zero vector', () => {
+    expect(Vector.Zero).toEqual(new Vector(0, 0))
+  })
+
+  it('should get a random vector', () => {
+    for (let i = 0; i < 100; i++) {
+      const vec = Vector.random(1, 2)
+
+      expect(vec.x).toBeGreaterThanOrEqual(0)
+      expect(vec.x).toBeLessThanOrEqual(1)
+      expect(vec.y).toBeGreaterThanOrEqual(0)
+      expect(vec.y).toBeLessThanOrEqual(2)
+    }
+  })
+
+  it('should get a vector with just an x value', () => {
+    expect(Vector.x(1)).toEqual(new Vector(1, 0))
+  })
+
+  it('should get a vector with just a y value', () => {
+    expect(Vector.y(1)).toEqual(new Vector(0, 1))
+  })
+})
