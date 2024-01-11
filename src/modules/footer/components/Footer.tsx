26cf50c21125ecc5945b229dcb0affcf71943f25
ArtificialLegacy <redsbro457@gmail.com>
2024-01-11 07:55:36 -0500

    add name and year to footer

--- src/modules/footer/components/Footer.tsx
+++ src/modules/footer/components/Footer.tsx
@@ -3,7 +3,7 @@ import '../styles/footer.css'
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer-copyright">&#169; Name??</p>
+      <p className="footer-copyright">&#169; 2024 Joseph Goyer</p>
       <a href="https://github.com/ArtificialLegacy">
         <img className="footer-github" src="GitHub_Logo_White.png" alt="Github Profile Link" />
       </a>
