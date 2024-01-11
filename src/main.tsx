5770ff72c7e43062f83905fe5bac488ccfafa724
ArtificialLegacy <redsbro457@gmail.com>
2024-01-11 03:54:59 -0500

    add reset.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './app.css'
+import './reset.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
