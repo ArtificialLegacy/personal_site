5e84dbca286183824042a35b6ae0b003ce003744
ArtificialLegacy <redsbro457@gmail.com>
2024-01-11 08:06:17 -0500

    adjust footer styles

--- src/modules/footer/components/Footer.tsx
+++ src/modules/footer/components/Footer.tsx
@@ -3,9 +3,9 @@ import '../styles/footer.css'
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer-copyright">&#169; 2024 Joseph Goyer</p>
+      <p>&#169; 2024 Joseph Goyer</p>
       <a href="https://github.com/ArtificialLegacy">
-        <img className="footer-github" src="GitHub_Logo_White.png" alt="Github Profile Link" />
+        <img src="GitHub_Logo_White.png" alt="Github Profile Link" />
       </a>
     </footer>
   )
