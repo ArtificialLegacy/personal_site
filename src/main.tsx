16175011a2aefdebd9eb9918ae59230dd2ed4b9e
ArtificialLegacy <redsbro457@gmail.com>
2024-01-11 07:02:34 -0500

    reverse import order of app.css and reset.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './app.css'
 import './reset.css'
+import './app.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
