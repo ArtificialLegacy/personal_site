f2923f27374726f4a4efd16298659b9d8a4e6cd0
ArtificialLegacy <redsbro457@gmail.com>
2024-01-11 07:20:52 -0500

    update tech list

--- src/modules/tech_list/components/TechList.tsx
+++ src/modules/tech_list/components/TechList.tsx
@@ -9,16 +9,19 @@ type TechListProps = {
 function TechList(props: TechListProps) {
   return (
     <div className="tech-list-panel">
-      <h3 className="tech-list-header">Technologies I Use</h3>
+      <header>
+        <div>
+          <h3>Technologies I Use</h3>
+        </div>
+      </header>
       <ul className="tech-list">
         {props.techs.map((tech) => (
           <li className="tech-list-item" key={tech.title}>
-            <p className="tech-list-title">
-              <img src={tech.icon} alt={`Logo for ${tech}.`} className="tech-list-icon" title={tech.title} />
-              {tech.title}
-            </p>
-            <div className="tech-list-bar" style={{ width: 75 * (tech.percentage / 100) + '%' }} />
-            <div className="tech-list-divider" />
+            <span className="tech-list-item-head">
+              <img src={tech.icon} alt={`Logo for ${tech}.`} title={tech.title} />
+              <h6>{tech.title}</h6>
+            </span>
+            <div style={{ width: 75 * (tech.percentage / 100) + '%' }} />
           </li>
         ))}
       </ul>
