2ecd0a6eff2d592d05bb0081a5e05b3623c77363
ArtificialLegacy <redsbro457@gmail.com>
2024-01-13 14:10:38 -0500

    update profile

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -3,7 +3,7 @@ const profile = {
   email: 'jgoyer3@gmail.com',
   phone: '508-769-4464',
   description: `
-    Full-stack web developer with expertise in React and TypeScript for frontend, and Node.js on the backend. 
+    Full-stack web developer with skills in React and TypeScript for frontend, and Node.js on the backend. 
     Certified in Web Applications from Quinsigamond Community College. 
     Have explored various frontend frameworks such as Vue.js in the past, and currently learning Go for backend development.
   `,
