4777af6bb0dc1c461c6992647b6978fd5f6ca63b
ArtificialLegacy <redsbro457@gmail.com>
2024-01-13 13:32:29 -0500

    update banner

---
+++ src/data/profile.ts
@@ -0,0 +1,11 @@
+const profile = {
+  name: 'Joseph Goyer',
+  email: 'jgoyer3@gmail.com',
+  description: `
+    Full-stack web developer with expertise in React and TypeScript for frontend, and Node.js on the backend. 
+    Certified in Web Applications from Quinsigamond Community College. 
+    Have explored various frontend frameworks such as Vue.js in the past, and currently learning Go for backend development.
+  `,
+}
+
+export default profile
